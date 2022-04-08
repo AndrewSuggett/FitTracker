1912f79694e77c180f32481d07e0e0d9484601d7
Andrews <andrewsuggett70@gmail.com>
2022-04-07 20:27:37 -0500

    First Unit Test

---
+++ FitTracker/Models/UserBodyStatistics.cs
@@ -0,0 +1,37 @@
+﻿namespace FitTracker.Models
+{
+    //Model
+    public class UserBodyStatistics
+    {
+        public int UserBodyStatisticsId { get; set; }
+
+        public double UserWeight { get; set; }
+
+        public double UserHeight { get; set; }
+
+    }
+
+
+    //
+    public class GetUserStatisitcs
+    {
+
+        private UserBodyStatistics userStats;
+        private double UserWeight { get; set; }
+        private double UserHeight { get; set; }
+
+        public GetUserStatisitcs(UserBodyStatistics userStats)
+        { 
+            this.userStats = userStats;
+            this.UserWeight = userStats.UserWeight;
+            this.UserHeight = userStats.UserHeight; 
+        }
+
+        public double GetUserWeightKg()
+        {
+            return UserWeight * 2.20462;
+        }
+
+
+    }
+}
---
+++ FitTracker/Tests/UnitTests/UserStatisticsTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using FitTracker.Models;
+
+namespace FitTracker.Tests.UnitTests
+{
+    public class UserStatisticsTests
+    {
+        [Fact]
+        public void UserWeightKg_Input1_ReturnKg()
+        {
+
+            UserBodyStatistics userStatsModel = new UserBodyStatistics { UserHeight = 1, UserWeight = 1};
+
+            GetUserStatisitcs userStats = new GetUserStatisitcs(userStatsModel);
+
+            Assert.Equal(userStats.GetUserWeightKg(), 1 * 2.20462);
+        }
+    }
+}
